4ab3b0d54aa04d7f7ea351335945542dbfe8b019
jjones <jimmy.jones@tutanota.com>
2023-10-23 16:48:57 -0600

    setup x an y values and labels

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import type { IrisItem } from '$lib/stores/IrisItem';
 	import { getContext } from 'svelte';
 
 	// grab the chart data from the store
@@ -12,6 +13,14 @@
 	// inner height and width of chart
 	const innerHeight = height - margin.top - margin.bottom;
 	const innerWidth = width - margin.left - margin.right;
+
+	// x and y axis values
+	const xValue = (item: IrisItem) => item.sepal_length;
+	const yValue = (item: IrisItem) => item.petal_width;
+
+	// x and y axis labels
+	const xAxisLabel = 'Sepal Length';
+	const yAxisLabel = 'Sepal Width';
 </script>
 
 <svg {width} {height}> <g transform={`translate(${margin.left}, ${margin.top})`} /></svg>
