4e2234dbd90301e854214694a2c7c04143905feb
jjones <jimmy.jones@tutanota.com>
2023-10-23 16:45:39 -0600

    setup svg dimensions and margins

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,11 @@
 
 	// grab the chart data from the store
 	const chartData: any = getContext('irisDataset');
+
+	// width, height and margins for svg container
+	const width: number = 960;
+	const height: number = 500;
+	const margin = { top: 20, right: 30, bottom: 50, left: 70 };
 </script>
 
-{JSON.stringify($chartData)}
+<svg {width} {height}> <g transform={`translate(${margin.left}, ${margin.top})`} /></svg>
