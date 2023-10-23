a190012f120a2bae050ce077f6f012c81ba21389
jjones <jimmy.jones@tutanota.com>
2023-10-23 17:03:40 -0600

    refactor to use talwiind for fill and stroke colors

--- src/lib/components/AxisLeft.svelte
+++ src/lib/components/AxisLeft.svelte
@@ -6,7 +6,7 @@
 
 {#each yScale.ticks() as tick, i}
 	<g transform={`translate(0, ${yScale(tick)})`}>
-		<line x2={innerWidth} stroke="#C0C0BB" />
-		<text fill="#635F5D" style="text-anchor: end;" dy=".32em" x={-tickOffset}>{tick}</text>
+		<line x2={innerWidth} class="stroke-slate-300" />
+		<text class="fill-slate-500" style="text-anchor: end;" dy=".32em" x={-tickOffset}>{tick}</text>
 	</g>
 {/each}
