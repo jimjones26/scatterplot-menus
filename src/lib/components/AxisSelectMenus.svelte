3079ba3356ecf3f5e16d16fa882c3c127d8836d8
jjones <jimmy.jones@tutanota.com>
2023-10-25 22:32:49 -0600

    move axis select menus to own component

---
+++ src/lib/components/AxisSelectMenus.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import * as Select from '$lib/components/ui/select';
+	import { dataStore } from '$lib/stores/data-store';
+
+	// grab the chart data from the store
+	const chartData: any = getContext('irisDataset');
+</script>
+
+<Select.Root
+	onSelectedChange={(item) => dataStore.updateSelectedX(item?.value)}
+	selected={$chartData.selectedX}
+>
+	<div class="flex">
+		<Select.Label>X:</Select.Label>
+		<Select.Trigger class="w-[180px]">
+			<Select.Value placeholder="Theme" />
+		</Select.Trigger>
+		<Select.Content>
+			<Select.Item value="light">Light</Select.Item>
+			<Select.Item value="dark">Dark</Select.Item>
+			<Select.Item value="system">System</Select.Item>
+		</Select.Content>
+	</div>
+</Select.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,7 @@
 	import AxisBottom from '$lib/components/AxisBottom.svelte';
 	import AxisLeft from '$lib/components/AxisLeft.svelte';
 	import Marks from '$lib/components/Marks.svelte';
-	import * as Select from '$lib/components/ui/select';
-	import { dataStore } from '$lib/stores/data-store';
+	import AxisSelectMenus from '$lib/components/AxisSelectMenus.svelte';
 
 	// grab the chart data from the store
 	const chartData: any = getContext('irisDataset');
@@ -43,22 +42,7 @@
 
 <div>
 	{$chartData.selectedX}
-	<Select.Root
-		onSelectedChange={(item) => dataStore.updateSelectedX(item?.value)}
-		selected={$chartData.selectedX}
-	>
-		<div class="flex">
-			<Select.Label>X:</Select.Label>
-			<Select.Trigger class="w-[180px]">
-				<Select.Value placeholder="Theme" />
-			</Select.Trigger>
-			<Select.Content>
-				<Select.Item value="light">Light</Select.Item>
-				<Select.Item value="dark">Dark</Select.Item>
-				<Select.Item value="system">System</Select.Item>
-			</Select.Content>
-		</div>
-	</Select.Root>
+	<AxisSelectMenus />
 	<svg {width} {height}>
 		<g transform={`translate(${margin.left}, ${margin.top})`}>
 			<AxisBottom {xScale} {innerHeight} tickOffset={10} />
